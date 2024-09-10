7c379d39b14d9c950742c671b89a0f61e58b4d25
Christopher Sy <christophersy@Christophers-Laptop.local>
2024-09-10 14:02:06 -0700

    Commit 1 - Server directory, initialize Spring backend via curl with Spring web, Spring Data JPA, and Postgres dependencies

---
+++ server/src/main/java/com/friendy/FriendyAppApplication.java
@@ -0,0 +1,13 @@
+package com.friendy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendyAppApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/friendy/FriendyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.friendy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
