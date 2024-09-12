308666a0d91a77ce79e10740e9b3cbde4b0774f6
Christopher Sy <christophersy@Christophers-Laptop.local>
2024-09-11 19:03:56 -0700

    Commit 8 - Add get method for single user via findById.

--- server/src/main/java/com/friendy/services/UserService.java
+++ server/src/main/java/com/friendy/services/UserService.java
@@ -25,4 +25,8 @@ public class UserService {
         return userRepository.findAll();
     }
 
+    public Optional<User> oneUser(Integer id){
+        return userRepository.findById(id);
+    }
+    
 }
