4c3e90146637e44c519a73ce0383e474144fd07e
Christopher Sy <christophersy.sigh@gmail.com>
2024-09-22 16:14:13 -0700

    Commit 45 - Create account page render accout form component.

--- client/src/components/AccountForm.jsx
+++ client/src/components/AccountForm.jsx
@@ -1,1 +1,72 @@
-// stage file strucutre
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const AccountForm = () => {
+    const navigate = useNavigate();
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+        username: '',
+        firstName: '',
+        lastName: '',
+        state: '',
+        city:'',
+    });
+
+    const handleChange = (e) => {
+        setFormData({...formData, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            await fetch("http://localhost:5500/create_friendy", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(formData)
+            });
+            //Maybe modify, adding modal popup - let user choose to move to login
+            navigate("/login");
+            console.log(formData);
+        } catch (error) {
+            console.error("Failed to submit form.", error);
+        };
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label>Email</label>
+                <input type="text" name="email" value="{formData.email}" onChange={handleChange} required/>
+            </div>
+            <div>
+                <label>Password</label>
+                <input type="text" name="password" value="{formData.password}" onChange={handleChange} required/>
+            </div>
+            <div>
+                <label>Username</label>
+                <input type="text" name="userName" value="{formData.username}" onChange={handleChange} required/>
+            </div>
+            <div>
+                <label>First Name</label>
+                <input type="text" name="firstName" value="{formData.firstname}" onChange={handleChange} required/>
+            </div>
+            <div>
+                <label>Last Name</label>
+                <input type="text" name="lastName" value="{formData.lastname}" onChange={handleChange} required/>
+            </div>
+            <div>
+                <label>State</label>
+                <input type="text" name="state" value="{formData.state}" onChange={handleChange} required/>
+            </div>
+            <div>
+                <label>City</label>
+                <input type="text" name="city" value="{formData.city}" onChange={handleChange} required/>
+            </div>
+            <button type="submit">Continue</button>
+        </form>
+    );
+};
+
+export default AccountForm;
--- client/src/pages/CreateAccount.jsx
+++ client/src/pages/CreateAccount.jsx
@@ -1,1 +1,14 @@
 //stage file structure
+import React from 'react';
+import AccountForm from "../components/AccountForm";
+
+const CreateAccount = () => {
+    return (
+        <div>
+            <h1>Create a Friendy Account</h1>
+            <AccountForm/>
+        </div>
+    );
+};
+
+export default CreateAccount;
